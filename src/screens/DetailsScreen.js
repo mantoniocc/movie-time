0da8673a9840b59767f4e40dacef8839f19c5415
Mario <marioantonio.cortes@gmail.com>
2021-02-07 21:45:04 -0300

    API Connection to the project

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 const DetailsScreen = ({navigation, route}) => {
-  console.log(route);
   const movie = route.params.movie;
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://www.omdbapi.com/?apikey=111404cd&t=star+wars&y=1977');
+    xhr.send();
+
+    xhr.onload = () => {
+      if(xhr.status == 200){
+        let response = JSON.parse(xhr.response);
+        setMovieDetails(response);
+      }else{
+        console.log(`HTTP Request Failed ${xhr.status}`);
+      }
+    };
+  }, []);
+
     return (
       <View style={styles.mainView}>
-        <Text style={{fontSize: 20}}>{movie.title} ({movie.release})</Text>
-        <Text style={{fontSize: 100}}>{movie.screenNumber}</Text>
-          <Button
-            title="Go to Image"
-            onPress={() => {navigation.navigate("BigImageView")}}/>
-          <Button
-            title="More Details"
-            onPress={() => {
-              movie.screenNumber = movie.screenNumber + 1;
-              console.log(movie);
-              navigation.push("Details_to_Details", {movie: movie})
-            }}/>
-          <Button
-            title="Go Back Home"
-            onPress={() => {navigation.popToTop()}}/>
+        <Text>{
+          (movieDetails == null ? "" : movieDetails.Title)
+        }</Text>
+        <Text>{
+          (movieDetails == null ? "" : movieDetails.Released)
+        }</Text>
+        <Text>{
+          (movieDetails == null ? "" : movieDetails.Plot)
+        }</Text>
       </View>
     )
 };
